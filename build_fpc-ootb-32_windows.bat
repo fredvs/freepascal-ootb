b0ab17b20d2452971bfa780bdff2a872f138a741
fredvs <fiens@hotmail.com>
2025-01-07 00:29:58 +0100

    Fix in build script for windows.

--- build_fpc-ootb-32_windows.bat
+++ build_fpc-ootb-32_windows.bat
@@ -6,13 +6,22 @@ COMPILER=fpc.exe
 INSTALLDIR=C:\fpc-ootb-tmp
 
 make clean
-rm -f -r C:\fpc-ootb-tmp
+
+if EXIST C:\fpc-ootb-tmp (
+del /f/s/q C:\fpc-ootb-tmp > nul
+rmdir /s/q C:\fpc-ootb-tmp
+)
+
 mkdir C:\fpc-ootb-tmp
 
 make all FPC=$COMPILER TARGET=i386-win32  
 make FPC=$COMPILER install INSTALL_PREFIX=$INSTALLDIR
 
-rm -f -r C:\fpc-ootb-32
+if EXIST C:\fpc-ootb-tmp (
+del /f/s/q C:\fpc-ootb-tmp > nul
+rmdir /s/q C:\fpc-ootb-tmp
+)
+
 mkdir C:\fpc-ootb-32
 mkdir C:\fpc-ootb-32\lib
 mkdir C:\fpc-ootb-32\license
@@ -20,23 +29,23 @@ mkdir C:\fpc-ootb-32\tools
 mkdir C:\fpc-ootb-32\units
 mkdir C:\fpc-ootb-32\units\i386-win32
 
-cp C:\ootb\fp.cfg C:\fpc-ootb-32\
-cp C:\ootb\fp.ini C:\fpc-ootb-32\
-cp C:\ootb\fpc.cfg C:\fpc-ootb-32\
-cp C:\ootb\readme.txt C:\fpc-ootb-32\
+xcopy /E /R /Y /Q installbin\*.* C:\codetyphon\
 
-cp -rf C:\license C:\fpc-ootb-32
 
-cp C:\fpc-ootb-tmp\usr\lib\fpc\3.2.4\ppcx64 C:\fpc-ootb-32\fpc-ootb-32
-cp C:\fpc-ootb-tmp\usr\bin\fp C:\fpc-ootb-32\fpide-ootb-64
-cp C:\fpc-ootb-tmp\usr\lib\libpas2jslib.so C:\fpc-ootb-32\lib\
-cp -rf C:\fpc-ootb-tmp\usr\lib\fpc\3.2.4\units\i386-win32 C:\fpc-ootb-32\units
-cp C:\fpc-ootb-tmp\usr\bin\fpcres C:\fpc-ootb-32\tools\
-cp C:\fpc-ootb-tmp\usr\bin\h2pas C:\fpc-ootb-32\tools\
-cp C:\fpc-ootb-tmp\usr\bin\fpcjres C:\fpc-ootb-32\tools\
-cp C:\fpc-ootb-tmp\usr\bin\pas2js C:\fpc-ootb-32\tools\
+xcopy /E /R /Y /Q C:\ootb\fp.cfg C:\fpc-ootb-32\
+xcopy /E /R /Y /Q C:\ootb\fp.ini C:\fpc-ootb-32\
+xcopy /E /R /Y /Q C:\ootb\fpc.cfg C:\fpc-ootb-32\
+xcopy /E /R /Y /Q C:\ootb\readme.txt C:\fpc-ootb-32\
 
-#tar -zcvf fpc-ootb-324_i386-win32.tar.gz C:\fpc-ootb-32
-rm -f -r C:\fpc-ootb-tmp
-#rm -f -r C:\fpc-ootb-32
+xcopy /E /R /Y /Q -rf C:\license C:\fpc-ootb-32
+
+xcopy /E /R /Y /Q C:\fpc-ootb-tmp\usr\lib\fpc\3.2.4\ppc386.exe C:\fpc-ootb-32\fpc-ootb-32.exe
+xcopy /E /R /Y /Q C:\fpc-ootb-tmp\usr\bin\fp.exe C:\fpc-ootb-32\fpide-ootb-64.exe
+xcopy /E /R /Y /Q C:\fpc-ootb-tmp\usr\lib\libpas2jslib.dll C:\fpc-ootb-32\lib\
+xcopy /E /R /Y /Q -rf C:\fpc-ootb-tmp\usr\lib\fpc\3.2.4\units\i386-win32 C:\fpc-ootb-32\units
+xcopy /E /R /Y /Q C:\fpc-ootb-tmp\usr\bin\fpcres.exe C:\fpc-ootb-32\tools\
+xcopy /E /R /Y /Q C:\fpc-ootb-tmp\usr\bin\h2pas.exe C:\fpc-ootb-32\tools\
+xcopy /E /R /Y /Q C:\fpc-ootb-tmp\usr\bin\fpcjres.exe C:\fpc-ootb-32\tools\
+xcopy /E /R /Y /Q C:\fpc-ootb-tmp\usr\bin\pas2js.exe C:\fpc-ootb-32\tools\
+
 
