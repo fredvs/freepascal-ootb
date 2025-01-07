74675c3441685eb65ae39069ad0058ac6cbc16f3
fredvs <fiens@hotmail.com>
2025-01-07 04:12:53 +0100

    Added build script for windows.

--- build_fpc-ootb-32_windows.bat
+++ build_fpc-ootb-32_windows.bat
@@ -1,10 +1,3 @@
-
-TARGET=i386-win32
-
-COMPILER=fpc.exe
-
-INSTALLDIR=C:\fpc-ootb-tmp
-
 make clean
 
 if EXIST C:\fpc-ootb-tmp (
@@ -14,35 +7,40 @@ rmdir /s/q C:\fpc-ootb-tmp
 
 mkdir C:\fpc-ootb-tmp
 
-make all FPC=$COMPILER TARGET=i386-win32  
-make FPC=$COMPILER install INSTALL_PREFIX=$INSTALLDIR
+make all FPC=fpc.exe TARGET=i386-win32  
+make FPC=fpc.exe install INSTALL_PREFIX=C:\fpc-ootb-tmp
 
-if EXIST fpc-ootb-32 (
-del /f/s/q fpc-ootb-32 > nul
-rmdir /s/q fpc-ootb-32
+if EXIST C:\fpc-ootb-32 (
+del /f/s/q C:\fpc-ootb-32 > nul
+rmdir /s/q C:\fpc-ootb-32
 )
 
-mkdir fpc-ootb-32
-mkdir fpc-ootb-32\lib
-mkdir fpc-ootb-32\license
-mkdir fpc-ootb-32\tools
-mkdir fpc-ootb-32\units
-mkdir fpc-ootb-32\units\i386-win32
+mkdir C:\fpc-ootb-32
+mkdir C:\fpc-ootb-32\lib
+mkdir C:\fpc-ootb-32\license
+mkdir C:\fpc-ootb-32\tools
+mkdir C:\fpc-ootb-32\units
+mkdir C:\fpc-ootb-32\units\i386-win32
 
 xcopy /E /R /Y /Q ootb\fp.cfg C:\fpc-ootb-32\
 xcopy /E /R /Y /Q ootb\fp.ini C:\fpc-ootb-32\
 xcopy /E /R /Y /Q ootb\fpc.cfg C:\fpc-ootb-32\
 xcopy /E /R /Y /Q ootb\readme.txt C:\fpc-ootb-32\
 
-xcopy /E /R /Y /Q -rf license C:\fpc-ootb-32
+xcopy /E /R /Y /Q /S license C:\fpc-ootb-32\license\
 
-xcopy /E /R /Y /Q C:\fpc-ootb-tmp\usr\lib\fpc\3.2.4\ppc386.exe C:\fpc-ootb-32\fpc-ootb-32.exe
-xcopy /E /R /Y /Q C:\fpc-ootb-tmp\usr\bin\fp.exe C:\fpc-ootb-32\fpide-ootb-64.exe
-xcopy /E /R /Y /Q C:\fpc-ootb-tmp\usr\lib\libpas2jslib.dll C:\fpc-ootb-32\lib\
-xcopy /E /R /Y /Q /S C:\fpc-ootb-tmp\usr\lib\fpc\3.2.4\units\i386-win32 C:\fpc-ootb-32\units
-xcopy /E /R /Y /Q C:\fpc-ootb-tmp\usr\bin\fpcres.exe C:\fpc-ootb-32\tools\
-xcopy /E /R /Y /Q C:\fpc-ootb-tmp\usr\bin\h2pas.exe C:\fpc-ootb-32\tools\
-xcopy /E /R /Y /Q C:\fpc-ootb-tmp\usr\bin\fpcjres.exe C:\fpc-ootb-32\tools\
-xcopy /E /R /Y /Q C:\fpc-ootb-tmp\usr\bin\pas2js.exe C:\fpc-ootb-32\tools\
+xcopy /E /R /Y /Q C:\fpc-ootb-tmp\bin\i386-win32\ppc386.exe C:\fpc-ootb-32\
+ren C:\fpc-ootb-32\ppc386.exe fpc-ootb-32.exe
+xcopy /E /R /Y /Q C:\fpc-ootb-tmp\bin\i386-win32\fp.exe C:\fpc-ootb-32\
+ren C:\fpc-ootb-32\fp.exe fpide-ootb-32.exe
+xcopy /E /R /Y /Q C:\fpc-ootb-tmp\bin\i386-win32\pas2jslib.dll C:\fpc-ootb-32\lib\
+xcopy /E /R /Y /Q /S C:\fpc-ootb-tmp\units\i386-win32 C:\fpc-ootb-32\units
+xcopy /E /R /Y /Q C:\fpc-ootb-tmp\bin\i386-win32\fpcres.exe C:\fpc-ootb-32\tools\
+xcopy /E /R /Y /Q C:\fpc-ootb-tmp\bin\i386-win32\h2pas.exe C:\fpc-ootb-32\tools\
+xcopy /E /R /Y /Q C:\fpc-ootb-tmp\bin\i386-win32\fpcjres.exe C:\fpc-ootb-32\tools\
+xcopy /E /R /Y /Q C:\fpc-ootb-tmp\bin\i386-win32\pas2js.exe C:\fpc-ootb-32\tools\
 
-
+if EXIST C:\fpc-ootb-tmp (
+del /f/s/q C:\fpc-ootb-tmp > nul
+rmdir /s/q C:\fpc-ootb-tmp
+)
