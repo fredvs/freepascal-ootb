4896d55a9ccb09b972d6322b1b02ae5919036375
fredvs <fiens@hotmail.com>
2025-05-26 23:43:57 +0200

    Fixed linking dependencies for OpenBSD.(2)

--- compiler/systems/t_bsd.pas
+++ compiler/systems/t_bsd.pas
@@ -368,8 +368,14 @@ begin
           begin
            i:=Pos(target_info.sharedlibext,S);
            if i>0 then
-              Insert(':',s,1);   // needed for the linker
-           LinkRes.Add('-l'+s);
+             
+            {$ifdef openbsd} 
+            Delete(S,i,255); Delete(S,i,255);
+            {$else}
+            Insert(':',s,1);   // needed for the linker
+            {$endif}
+
+             LinkRes.Add('-l'+s);
          end
         else
          begin
